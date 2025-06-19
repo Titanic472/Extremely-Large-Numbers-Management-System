6c27d1333650d143ffeb5c792a80ba29ba30b59c
Titanic472 <ctac472@gmail.com>
2025-06-19 20:05:53 +0200

    Added substraction Added full negative numbers support Added even more comparsion operators Added Absolute value function

--- Extremely Large Numbers.cpp
+++ Extremely Large Numbers.cpp
@@ -5,50 +5,36 @@
 
 using namespace std;
 
-struct ExtremeNumber{//finish with numbers smaller than 0, division, substraction and comparsion
+struct ExtremeNumber{//finish with substraction
     vector<int> Number;
     bool negative = false;
 
-    void Set(string NumberString){
-        if(NumberString.length()==0){
+    void Set(string numberString){
+        if(numberString.length()==0){
                 cout << "\"Set\" Error: empty string\n";
                 return;
         }
 
-        if(NumberString[0]=='-'){
-            NumberString.erase(0, 1);
+        if(numberString[0]=='-'){
+            numberString.erase(0, 1);
             negative = true;
         }
         else negative = false;
 
         Number.clear();
-        int size = (NumberString.length()-1)/3+1;
+        int size = (numberString.length()-1)/3+1;
         for(int i = 0; i<size; ++i)Number.push_back(0);
         //cout << "Resized: " <<Number[0]+1 << "\n";
+        while(numberString.length()%3>0)numberString = "0" + numberString;
 
-        while(NumberString.length()%3>0)NumberString = "0" + NumberString;
-
-        for(int i = Number.size()-1; i>=0 && NumberString !=""; --i){
+        for(int i = Number.size()-1; i>=0 && numberString !=""; --i){
             string Temp = "";
-            for(int i = 0; i<3;++i)Temp += NumberString[i];
+            for(int i = 0; i<3;++i)Temp += numberString[i];
             //cout << Temp << " ";
             Number[i] = stoi(Temp);
             //cout << stoi(Temp) << "\n";
-            NumberString.erase(0, 3);
+            numberString.erase(0, 3);
         }
-    }
-
-    void SetSegment(int Value, int PositionIndex){
-        //cout << "Index: " << PositionIndex << "\n";
-        while(PositionIndex>Number.size()-1)Number.push_back(0);
-        Number[PositionIndex] = Value;
-    }
-
-    void AddToSegment(int Value, int PositionIndex){
-        //cout << "Index: " << PositionIndex << "\n";
-        while(PositionIndex>(int)(Number.size()-1))Number.push_back(0);
-        // cout << Number.size();
-        Number[PositionIndex] += Value;
     }
 
     void Print(bool NewLine=true, bool segmentedString = false){
@@ -73,6 +59,21 @@ struct ExtremeNumber{//finish with numbers smaller than 0, division, substractio
         return stringNumber;
     }
 
+    private:
+
+    void SetSegment(int value, int positionIndex){
+        //cout << "Index: " << positionIndex << "\n";
+        while(positionIndex>Number.size()-1)Number.push_back(0);
+        Number[positionIndex] = value;
+    }
+
+    void AddToSegment(int value, int positionIndex){
+        //cout << "Index: " << positionIndex << "\n";
+        while(positionIndex>(int)(Number.size()-1))Number.push_back(0);
+        // cout << Number.size();
+        Number[positionIndex] += value;
+    }
+
     void Rearrange(){
         for(int i = 0; i<Number.size(); ++i){
             if(Number[i]>999){
@@ -80,28 +81,123 @@ struct ExtremeNumber{//finish with numbers smaller than 0, division, substractio
                     SetSegment(Number[i]%1000,i);
             }
         }
+        while(Number.size()>1 && Number[Number.size()-1]==0)Number.pop_back();
     }
-
-    ExtremeNumber operator+(const ExtremeNumber& SecondNumber){
-        int size = max(SecondNumber.Number.size(), Number.size());
-        //for(int i = Number.size(); i<SecondNumber.Number.size(); ++i)Number.push_back(0);
-        //cout << "Size: " << Number.size() << "\n";
-        for(int i = 0; i<size; ++i){
-            if(SecondNumber.Number.size()>i)AddToSegment(SecondNumber.Number[i], i);
+    
+    int Compare(const ExtremeNumber& number1, const ExtremeNumber& number2){//1 - first is bigger, 2 - second is bigger, 0 - they are equal
+        if(number1.negative && !number2.negative)return 2;
+        else if(!number1.negative && number2.negative) return 1;
+        //cout << "Passed minus comparsion\n";
+        if(number1.Number.size()>number2.Number.size())return 1;
+        else if(number1.Number.size()<number2.Number.size()) return 2;
+        //cout << "Passed size comparsion\n";
+        for(int i = number1.Number.size()-1; i>=0; --i){
+            if(number1.Number[i]>number2.Number[i]){
+                    return 1;
+            }
+            else if(number1.Number[i]<number2.Number[i])return 2;
         }
-        Rearrange();
-        return *this;
+        //cout << "Passed all comparsions\n";
+        return 0;
     }
 
-    ExtremeNumber operator+=(const ExtremeNumber& SecondNumber){
-        return *this + SecondNumber;
+    ExtremeNumber Add(const ExtremeNumber& secondNumber, bool reverse = false){
+        bool secondNumberNegative = secondNumber.negative;
+        if(reverse){
+            secondNumberNegative = !secondNumberNegative;
+        }
+
+        if(negative == secondNumberNegative){
+            int size = max(secondNumber.Number.size(), Number.size());
+            //for(int i = Number.size(); i<secondNumber.Number.size(); ++i)Number.push_back(0);
+            //cout << "Size: " << Number.size() << "\n";
+            for(int i = 0; i<size; ++i){
+                if(secondNumber.Number.size()>i)AddToSegment(secondNumber.Number[i], i);
+            }
+            Rearrange();
+            return *this;
+        }
+        else{
+            if(Abs(*this)==Abs(secondNumber)){
+                Number.clear();
+                Number.push_back(0);
+                negative = false;
+                return *this;
+            }
+            //No need in comparsion, because in this moment second number absolute value is smaller than value of *this(compared in operator functions)
+
+            for(int i = 0; i < secondNumber.Number.size(); ++i){
+                if(Number[i] < secondNumber.Number[i]){
+                    --Number[i+1];
+                    Number[i] += 1000 - secondNumber.Number[i];
+                }
+                else Number[i] -= secondNumber.Number[i];
+            }
+            Rearrange();
+            return *this;
+        }
     }
 
-    ExtremeNumber operator*(const ExtremeNumber& SecondNumber){
+    public:
+
+    ExtremeNumber Abs(ExtremeNumber operand){
+        operand.negative = false;
+        return operand;
+    }
+
+    ExtremeNumber operator+(const ExtremeNumber& secondNumber){
+        ExtremeNumber temp;
+        if(Abs(*this)<Abs(secondNumber)){
+            temp = secondNumber;
+            return temp.Add(*this);
+        }
+        else{
+            temp = *this;
+            return temp.Add(secondNumber);
+        }
+    }
+
+    ExtremeNumber operator+=(const ExtremeNumber& secondNumber){
+        if(Abs(*this)<Abs(secondNumber)){
+            ExtremeNumber temp = *this;
+            *this = secondNumber;
+            return Add(temp);
+        }
+        else{
+            return Add(secondNumber);
+        }
+    }
+
+    ExtremeNumber operator-(const ExtremeNumber& secondNumber){
+        ExtremeNumber temp;
+        if(Abs(*this)<Abs(secondNumber)){
+            temp = secondNumber;
+            temp.negative = !temp.negative;
+            return temp.Add(*this);
+        }
+        else{
+            temp = *this;
+            return temp.Add(secondNumber, true);
+        }
+    }
+
+    ExtremeNumber operator-=(const ExtremeNumber& secondNumber){
+        if(Abs(*this)<Abs(secondNumber)){
+            ExtremeNumber temp = *this;
+            *this = secondNumber;
+            negative = !negative;
+            return Add(temp);
+        }
+        else{
+            return Add(secondNumber, true);
+        }
+    }
+
+    ExtremeNumber operator*(const ExtremeNumber& secondNumber){
         ExtremeNumber temp;
         for(int i = 0; i<Number.size(); ++i){
-            for(int j = 0; j<SecondNumber.Number.size(); ++j){
-                temp.AddToSegment(Number[i]*SecondNumber.Number[j], j+i);
+            for(int j = 0; j<secondNumber.Number.size(); ++j){
+                temp.AddToSegment(Number[i]*secondNumber.Number[j], j+i);
                 if(temp.Number[j+i]>2140000){
                     temp.AddToSegment(temp.Number[j+i]/1000,j+i+1);
                     temp.SetSegment(temp.Number[j+i]%1000, i+j);
@@ -109,6 +205,7 @@ struct ExtremeNumber{//finish with numbers smaller than 0, division, substractio
             }
         }
         temp.Rearrange();
+        temp.negative = negative ^ secondNumber.negative;
         return temp;
     }
 
@@ -117,98 +214,126 @@ struct ExtremeNumber{//finish with numbers smaller than 0, division, substractio
         return *this;
     }
 
-    int Compare(const ExtremeNumber& Number1, const ExtremeNumber& Number2){//1 - first is bigger, 2 - second is bigger, 0 - they are equal
-        if(Number1.negative && !Number2.negative)return 2;
-        else if(!Number1.negative && Number2.negative) return 1;
-        //cout << "Passed minus comparsion\n";
-        if(Number1.Number.size()>Number2.Number.size())return 1;
-        else if(Number1.Number.size()<Number2.Number.size()) return 2;
-        //cout << "Passed size comparsion\n";
-        for(int i = Number1.Number.size()-1; i>=0; --i){
-            if(Number1.Number[i]>Number2.Number[i]){
-                    return 1;
-            }
-            else if(Number1.Number[i]<Number2.Number[i])return 2;
-        }
-        //cout << "Passed all comparsions\n";
-        return 0;
+    ExtremeNumber operator*(const int& secondNumber){
+        ExtremeNumber N2;
+        N2.Set(to_string(secondNumber));
+        return *this * N2;
+    }
+
+    ExtremeNumber operator*=(int& secondNumber){
+        ExtremeNumber N2;
+        N2.Set(to_string(secondNumber));
+        *this = *this * N2;
+        return *this;
     }
 
     //Comparsion operators
-    bool operator>(const ExtremeNumber& Number2){
-        int Value = Compare(*this, Number2);
-        if(Value == 1)return true;
+    bool operator>(const ExtremeNumber& number2){
+        int value = Compare(*this, number2);
+        if(value == 1)return true;
         else return false;
     }
 
-    bool operator>=(const ExtremeNumber& Number2){
-        int Value = Compare(*this, Number2);
-        if(Value == 2)return false;
+    bool operator>=(const ExtremeNumber& number2){
+        int value = Compare(*this, number2);
+        if(value == 2)return false;
         else return true;
     }
 
-    bool operator<(const ExtremeNumber& Number2){
-        int Value = Compare(Number2, *this);
-        if(Value == 1)return true;
+    bool operator<(const ExtremeNumber& number2){
+        int value = Compare(number2, *this);
+        if(value == 1)return true;
         else return false;
     }
 
-    bool operator<=(const ExtremeNumber& Number2){
-        int Value = Compare(Number2, *this);
-        if(Value == 2)return false;
+    bool operator<=(const ExtremeNumber& number2){
+        int value = Compare(number2, *this);
+        if(value == 2)return false;
+        else return true;
+    }
+
+    bool operator==(const ExtremeNumber& number2){
+        int value = Compare(number2, *this);
+        if(value == 0)return true;
+        else return false;
+    }
+
+    bool operator!=(const ExtremeNumber& number2){
+        int value = Compare(number2, *this);
+        if(value == 0)return false;
         else return true;
     }
     //End of Comparsion operators
 
     //Comparsions with int
-    bool operator<(const int Number2){
+    bool operator<(const int number2){
         ExtremeNumber N2;
-        N2.Set(to_string(Number2));
+        N2.Set(to_string(number2));
         return *this < N2;
     }
 
-    bool operator<=(const int Number2){
+    bool operator<=(const int number2){
         ExtremeNumber N2;
-        N2.Set(to_string(Number2));
+        N2.Set(to_string(number2));
         return *this <= N2;
     }
 
-    bool operator>(const int Number2){
+    bool operator>(const int number2){
         ExtremeNumber N2;
-        N2.Set(to_string(Number2));
+        N2.Set(to_string(number2));
         return *this > N2;
     }
 
-    bool operator>=(const int Number2){
+    bool operator>=(const int number2){
         ExtremeNumber N2;
-        N2.Set(to_string(Number2));
+        N2.Set(to_string(number2));
         return *this >= N2;
     }
 
+    bool operator==(const int number2){
+        ExtremeNumber N2;
+        N2.Set(to_string(number2));
+        return *this == N2;
+    }
+
+    bool operator!=(const int number2) {
+        return !(*this == number2);  
+    }
+    //End of comparsions with int
 };
 
-bool operator>(const int Number1, const ExtremeNumber& Number2) {
+bool operator>(const int number1, const ExtremeNumber& number2) {
     ExtremeNumber N1;
-    N1.Set(to_string(Number1)); 
-    return N1 > Number2;  
+    N1.Set(to_string(number1)); 
+    return N1 > number2;  
 }
 
-bool operator>=(const int Number1, const ExtremeNumber& Number2) {
+bool operator>=(const int number1, const ExtremeNumber& number2) {
     ExtremeNumber N1;
-    N1.Set(to_string(Number1)); 
-    return N1 >= Number2;  
+    N1.Set(to_string(number1)); 
+    return N1 >= number2;  
 }
 
-bool operator<(const int Number1, const ExtremeNumber& Number2) {
+bool operator<(const int number1, const ExtremeNumber& number2) {
     ExtremeNumber N1;
-    N1.Set(to_string(Number1)); 
-    return N1 < Number2;  
+    N1.Set(to_string(number1)); 
+    return N1 < number2;  
 }
 
-bool operator<=(const int Number1, const ExtremeNumber& Number2) {
+bool operator<=(const int number1, const ExtremeNumber& number2) {
     ExtremeNumber N1;
-    N1.Set(to_string(Number1)); 
-    return N1 <= Number2;  
+    N1.Set(to_string(number1)); 
+    return N1 <= number2;  
+}
+
+bool operator==(const int number1, const ExtremeNumber& number2) {
+    ExtremeNumber N1;
+    N1.Set(to_string(number1)); 
+    return N1 == number2;  
+}
+
+bool operator!=(const int number1, const ExtremeNumber& number2) {
+    return !(number1 == number2);  
 }
 
 
@@ -216,22 +341,55 @@ int main(){
     ofstream File;
     File.open("Numbers.txt");
     srand(time(NULL));
-    ExtremeNumber Number, Number2, Cont;
-    Number.Set("9");
-    Number2.Set("1");
+    ExtremeNumber number, number2, Cont;
+    number.Set("9");
+    number2.Set("11");
+    Cont.Set("-1");
+    Cont -= number2;
+    number.Print();
+    number2.Print();
+
+    int a, b;
+    for(int i = 0; i<1000000; ++i){
+        a = rand()%1000000000;
+        b = rand()%1000000000;
+        // cout << "a = " << a << "\nb = " << b << "\n";
+        number.Set(to_string(a));
+        number2.Set(to_string(b));
+        // number.Print();
+        // number2.Print();
+        Cont = number - number2*(-1);
+        if(Cont != a-b*(-1))cout << "Error: " << a << " - " << b << " = " << Cont.GetString() << "\n";
+        Cont = number2 - number;
+        if(Cont != b-a)cout << "Error: " << b << " - " << a << " = " << Cont.GetString() << "\n";
+        Cont = number - number2*(-1);
+        if(Cont != a-b*(-1))cout << "Error: " << a << " - " << b*(-1) << " = " << Cont.GetString() << "\n";
+        Cont = number2 - number*(-1);
+        if(Cont != b-a*(-1))cout << "Error: " << b << " - " << a*(-1) << " = " << Cont.GetString() << "\n";
+        Cont = number*(-1) - number2;
+        if(Cont != a*(-1)-b)cout << "Error: " << a*(-1) << " - " << b << " = " << Cont.GetString() << "\n";
+        Cont = number2*(-1) - number;
+        if(Cont != b*(-1)-a)cout << "Error: " << b*(-1) << " - " << a << " = " << Cont.GetString() << "\n";
+    }
+
+    //for(int i = 0; i<10000000; ++i)Cont = number - number2;
+    Cont.Print();
+    /*Number.Set("9");
+    number2.Set("1");
+    
     for(int i = 1; i<=3; ++i){
         Cont = Number;
-            for(int j = 1; j<=Number2; ++j){
+            for(int j = 1; j<=number2; ++j){
                 if(j==10000)break;
-                Number=Number2*Number;
+                Number=number2*Number;
                 cout << j << " " << Number.Number.size() << "\n";
                 File << Number.GetString() << "\n";
             }
-        Number2=Number;
+        number2=Number;
         cout << "i = " << i << "\n";
         Number.Print();
         Number = Cont;
-    }
+    }*/
 
     return 0;
 }
--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # Extremely-Large-Numbers-Management-System
-Data structure that allows you to have numbers larger than int64 in C++. 
-Now it includes only comparsion, addition and multiplication. Sooner it will have more capabilities.
+Data structure that allows you to have numbers larger than int64 in C++.
+
+Speed is not very fast, but it is good enough for small programs.
+Later I will add comparsion with Python...
+
+It is based on the fact that numbers are stored in the form of a vector of digits.
+
+Now it includes:
+- comparsion, 
+- addition 
+- subtraction 
+- multiplication
+
+Math operations:
+- Absolute value of number
+
+More capabilities will be added sooner.
